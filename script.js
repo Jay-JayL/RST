25fe392e5c94e2c95964a63950593c4c25e442fb
Jay.Jay Lesperance <jay.jay.lesperance@ocsbstudent.ca>
2021-03-04 15:51:51 +0000

    Spaces in JS

--- script.js
+++ script.js
@@ -11,9 +11,8 @@ function area () {
 
   r = parseInt(r)
   h = parseInt(h)
-  
-    number = Math.PI * r * (r + Math.sqrt(h ** 2 + r ** 2))
-    number2 = Math.PI * (r ** 2) * (h / 3)
-    document.getElementById('paragraph').innerHTML = number
-     document.getElementById('paragraph2').innerHTML = number2
- }
+  number = Math.PI * r * (r + Math.sqrt(h ** 2 + r ** 2))
+  number2 = Math.PI * (r ** 2) * (h / 3)
+  document.getElementById('paragraph').innerHTML = number
+  document.getElementById('paragraph2').innerHTML = number2
+  }
