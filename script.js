438db4349b359a8530ffd37d70ce52f124decdca
Jay.Jay Lesperance <jay.jay.lesperance@ocsbstudent.ca>
2021-03-04 14:20:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let input = 0
+let input2 = 0
+let r = 0
+let h = 0
+let number = 0
+let number2 = 0
+
+document.getElementById('button').addEventListener('click', area)
+
+function area () {
+  r = document.getElementById('input').value
+  h = document.getElementById('input2').value
+
+  r = parseInt(r)
+  h = parseInt(h)
+
+  while (r == parseInt(r),h == parseInt(h)) {
+    function calulating () {
+    number = Math.PI(r)(r+Math.sqrt(h**2+r**2))
+    number2 = Math.PI(r**2)(h/3)
+    document.getElementById('paragraph').innerHTML = number
+    document.getElementById('paragraph2').innerHTML = number2
+  }} 
+}
